0b41ae13fa5ec871ac2452643ddde1304a260d42
Rafii2198 <rafii2198@gmail.com>
2020-10-07 01:06:03 +0200

    Alpha 0.1 - pre revamp

---
+++ informatyka/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <curses.h>
+
+using namespace std;
+
+string test;
+
+int main(){
+
+    cout  << "Witamaaaaa";
+    cout << endl << "Nie wiem ";
+    cin >> test;
+    return 0;
+
+}
