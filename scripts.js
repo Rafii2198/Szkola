abf3bd478d5b619f61ee00de5d593e91b82a1b31
Rafii2198 <rafii2198@gmail.com>
2020-11-14 20:48:56 +0100

    Page 1.0 - Design completed with Bootstrap 4 framework

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-var slider = document.getElementById("skala");
-var size = 0;
-slider.oninput = function() {
-    size = slider.value / 50;
-    document.getElementById("main").style.transform = 'scale('+size+')'
-}
+const queryString = window.location.search;
+const ipage = document.getElementById("page");
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.has('page')){
+    const page = urlParams.get('page');
+    ipage.src = page;
 
+}
