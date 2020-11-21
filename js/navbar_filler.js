0717d94009b873a39ef29ec9d25f02461015788b
Rafii2198 <rafii2198@gmail.com>
2020-11-21 11:58:39 +0100

    v1.1 - Automated Navbar + Start page

---
+++ js/navbar_filler.js
@@ -0,0 +1,30 @@
+fetch('/lista.json')
+	.then((response) => response.text())
+	.then((data) => {
+		filling(JSON.parse(data));
+	});
+
+function filling(lista) {
+	let nav_pai = document.getElementById('pai_lista');
+	let nav_wiai = document.getElementById('wiai_lista');
+	let lista_pai = lista['pai'];
+	let lista_wiai = lista['wiai'];
+	lista_pai.forEach((element) => {
+		console.log(element);
+		let entry = document.createElement('a');
+		let title = document.createTextNode(element['title']);
+		entry.className = 'dropdown-item';
+		entry.href = element['location'];
+		nav_pai.appendChild(entry);
+		entry.appendChild(title);
+	});
+	lista_wiai.forEach((element) => {
+		console.log(element);
+		let entry = document.createElement('a');
+		let title = document.createTextNode(element['title']);
+		entry.className = 'dropdown-item';
+		entry.href = element['location'];
+		nav_pai.appendChild(entry);
+		entry.appendChild(title);
+	});
+}
--- scripts.js
+++
@@ -1,8 +0,0 @@
-const queryString = window.location.search;
-const ipage = document.getElementById("page");
-const urlParams = new URLSearchParams(queryString);
-if (urlParams.has('page')){
-    const page = urlParams.get('page');
-    ipage.src = page;
-
-}
